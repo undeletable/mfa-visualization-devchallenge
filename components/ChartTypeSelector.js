47a59d9b6894e6b63366d09a1f21b4414dfce56b
Max Shepel <max@undeletable.name>
2024-09-30 18:07:56 +0300

    Add constants for charts

--- components/ChartTypeSelector.js
+++ components/ChartTypeSelector.js
@@ -1,27 +1,8 @@
+import { CHART_TYPES_SELECT_OPTIONS } from "../constants/charts.js";
 import { WebComponent } from "../lib/WebComponent.js";
 import { setChartType } from "../state/state.js";
 
 class ChartTypeSelector extends WebComponent {
-    options = [
-        {
-            isInitialSelection: true,
-            label: "Select chart type",
-            value: "",
-        },
-        {
-            label: "Line chart",
-            value: "line"
-        },
-        {
-            label: "Bar chart",
-            value: "bar"
-        },
-        {
-            label: "Pie chart",
-            value: "pie"
-        }
-    ];
-
     selectId = "chart-type-select";
 
     onConnected() {
@@ -35,10 +16,17 @@ class ChartTypeSelector extends WebComponent {
     render() {
         return `
             <select id=${this.selectId}>
-                ${this.options.map(({ isInitialSelection, label, value }) =>
-                    `<option value="${value}"${isInitialSelection ? " selected" : ""}>
-                        ${label}
-                    </option>`
+                ${CHART_TYPES_SELECT_OPTIONS.map(
+                    ({isDisabled, isInitialSelection, label, value }) =>
+                        `<option
+                            value="${value}"${
+                                isInitialSelection ? " selected" : ""
+                            }${
+                                isDisabled ? " disabled" : ""
+                            }
+                        >
+                            ${label}
+                        </option>`
                 )}
             </select>
         `;
