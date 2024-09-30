4ed29b1c9703e8be836d19aae93050b3f93f1f5b
Max Shepel <max@undeletable.name>
2024-09-30 18:14:34 +0300

    Add color constants

---
+++ constants/styles.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    accent: "#FFD700",
+    chart: ["#003366", "#0056A2", "#FFD700", "#F1F1F1", "#D32F2F"],
+    error: "#D32F2F",
+    gray: "#F1F1F1",
+    primary: "#003366",
+    secondary: "#0056A2",
+    text: "#000000",
+    white: "#FFFFFF"
+};
+
+export { COLORS };
